fd59a0c8d2be250d1d647e754e526ef9e7ba0bb7
Craig N. Caroon <ccaroon@webassign.net>
2013-05-21 12:00:22 -0400

    * preview is not working. trying to get it working and some code formattting   changes

--- jira.js
+++ jira.js
@@ -4,8 +4,7 @@ CmdUtils.CreateCommand
         names: ["jira"],
         icon: "https://staff.webassign.net/jira/favicon.ico",
         arguments: [{role: 'object', nountype: noun_arb_text}],
-        _link: function jira_link(jkey, type)
-        {
+        _link: function jira__link(jkey, type) {
             if (jkey.match(/^\d+$/))
             {
                 jkey = "YETI-"+jkey;
@@ -14,58 +13,57 @@ CmdUtils.CreateCommand
             var url = "http://webassign.atlassian.net/browse/"+jkey;
             if (type == 'xml')
             {
-                url = "http://webassign.atlassian.net/si/jira.issueviews:issue-xml/"+jkey+"/"+jkey+".xml";
+                url = "https://webassign.atlassian.net/si/jira.issueviews:issue-xml/"+jkey+"/"+jkey+".xml";
             }
             else
             {
-                url = "http://webassign.atlassian.net/browse/"+jkey;
+                url = "https://webassign.atlassian.net/browse/"+jkey;
             }
 
             return (url);
         },
-        preview: function preview(pblock, args)
-        {
-            var url = this._link(args.object.text,'xml');
-            CmdUtils.previewGet(pblock, url,
-                function (data)
-                {
-                    var item  = data.getElementsByTagName("item")[0];
-                    var title = item.getElementsByTagName("title")[0].textContent;
-                    var link  = item.getElementsByTagName("link")[0].textContent;
-                    var desc  = item.getElementsByTagName("description")[0].textContent;
-                    var assign = item.getElementsByTagName("assignee")[0].textContent;
-                    var priority = item.getElementsByTagName("priority")[0];
-                    var pimg = priority.attributes.getNamedItem('iconUrl').textContent;
-                    
-                    var status = item.getElementsByTagName("status")[0];
-                    var simg = status.attributes.getNamedItem('iconUrl').textContent;
-                    
-                    var type = item.getElementsByTagName("type")[0];
-                    var timg = type.attributes.getNamedItem('iconUrl').textContent;
+        preview: function jira__preview(pblock, args) {         
+            pblock.innerHTML =  this._link(args.object.text);
 
-                    var tmpl = "\
-                    <a href='${link}'>${title}</a>\
-                    <p>${ptxt} ${ttxt} - <img src='${pimg}'/><img src='${timg}'/><img src='${simg}'/><br>Assigned to: ${assign} - ${stxt}</p>\
-                    <p>${desc}</p>\
-                    ";
-                    pblock.innerHTML = CmdUtils.renderTemplate(tmpl,
-                        {
-                            title: title,
-                            link: link,
-                            desc: desc,
-                            ptxt: priority.textContent,
-                            pimg: pimg,
-                            stxt: status.textContent,
-                            simg: simg,
-                            ttxt: type.textContent,
-                            timg: timg,
-                            assign: assign,
-                        });
-                },
-                'xml');
+            var url = this._link(args.object.text,'xml');
+            CmdUtils.previewGet(pblock, url, function (data) {
+                pblock.innerHTML = "Found it!!!";
+                //var item  = data.getElementsByTagName("item")[0];
+                //var title = item.getElementsByTagName("title")[0].textContent;
+                //var link  = item.getElementsByTagName("link")[0].textContent;
+                //var desc  = item.getElementsByTagName("description")[0].textContent;
+                //var assign = item.getElementsByTagName("assignee")[0].textContent;
+                //var priority = item.getElementsByTagName("priority")[0];
+                //var pimg = priority.attributes.getNamedItem('iconUrl').textContent;
+                //
+                //var status = item.getElementsByTagName("status")[0];
+                //var simg = status.attributes.getNamedItem('iconUrl').textContent;
+                //
+                //var type = item.getElementsByTagName("type")[0];
+                //var timg = type.attributes.getNamedItem('iconUrl').textContent;
+                //
+                //var tmpl = "\
+                //<a href='${link}'>${title}</a>\
+                //<p>${ptxt} ${ttxt} - <img src='${pimg}'/><img src='${timg}'/><img src='${simg}'/><br>Assigned to: ${assign} - ${stxt}</p>\
+                //<p>${desc}</p>\
+                //";
+                //pblock.innerHTML = CmdUtils.renderTemplate(tmpl,
+                //    {
+                //        title: title,
+                //        link: link,
+                //        desc: desc,
+                //        ptxt: priority.textContent,
+                //        pimg: pimg,
+                //        stxt: status.textContent,
+                //        simg: simg,
+                //        ttxt: type.textContent,
+                //        timg: timg,
+                //        assign: assign,
+                //    });
+                }
+            );
         },
-        execute: function execute(args)
-        {
+        execute: function jira__execute(args) {
             Utils.openUrlInBrowser(this._link(args.object.text, 'html'));
         }
     }
